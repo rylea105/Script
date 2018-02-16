729b4d43896c9c0d3f9c1abae1dec19326069fcd
nylea <13538834+rylea105@users.noreply.github.com>
2018-02-16 22:22:12 +0700

    Create install-nginx-nodejs-pm2-yarn-git.sh

---
+++ install-nginx-nodejs-pm2-yarn-git.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#install epel-release
+yum install -y epel-release
+
+#install nginx
+yum install -y nginx
+systemctl start nginx
+systemctl enable nginx
+
+#install git
+yum install -y git
+
+#install node
+curl --silent --location https://rpm.nodesource.com/setup_8.x | sudo bash -
+yum -y install nodejs
+
+#open to connect http
+setsebool -P httpd_can_network_connect 1
